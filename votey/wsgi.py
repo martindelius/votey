2855121c8cace11f83eb852ddebf1e21bb983d10
Martin Delius <mail@martindelius.de>
2016-11-23 12:57:33 +0100

    fixed python app constant

--- votey/wsgi.py
+++ votey/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/var/www/votey/votey')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "votey.settings")
 
 application = get_wsgi_application()
